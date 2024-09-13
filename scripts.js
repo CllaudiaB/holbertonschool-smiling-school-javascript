4aab719e2a8cfa18645ef50f720a23cae3fff3aa
CllaudiaB <buraanna27@gmail.com>
2024-09-13 11:30:40 +0200

    task 3

--- scripts.js
+++ scripts.js
@@ -58,3 +58,150 @@ function addCarousselCard(data) {
   }
   
   listQuotes();
+
+
+  function addCarouselCardTutorials(data) {
+    const carouselInner = $("#carouselExampleControls3 .carousel-inner");
+  
+
+    carouselInner.empty();
+  
+
+    data.forEach((video, index) => {
+
+      const activeClass = index === 0 ? 'active' : '';
+  
+
+      const carouselItem = $(`
+        <div class="carousel-item ${activeClass}">
+          <div class="row align-items-center mx-auto">
+            <div class="col-12 d-flex justify-content-center">
+              <div class="card">
+                <img src="${video.thumbnail_url}" class="card-img-top" alt="Video thumbnail">
+                <div class="card-img-overlay text-center">
+                  <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">${video.title}</h5>
+                  <p class="card-text text-muted">${video.description}</p>
+                  <div class="creator d-flex align-items-center">
+                    <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                    <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                  </div>
+                  <div class="info pt-3 d-flex justify-content-between">
+                    <div class="rating">
+                      ${'<img src="images/star_on.png" alt="star on" width="15px">'.repeat(video.rating)}
+                      ${'<img src="images/star_off.png" alt="star off" width="15px">'.repeat(5 - video.rating)}
+                    </div>
+                    <span class="main-color">${video.duration}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `);
+  
+      carouselInner.append(carouselItem);
+    });
+  }
+  
+  function listPopularTutorials() {
+    const url = "https://smileschool-api.hbtn.info/popular-tutorials";
+  
+    $(document).ready(function () {
+      $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        crossDomain: true,
+        beforeSend: function () {
+          $(".loader").show();
+        },
+        success: function () {
+          $(".loader").hide();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.error("Error fetching data:", textStatus, errorThrown);
+          $(".loader").hide();
+        }
+      }).done(function (data) {
+        if (Array.isArray(data) && data.length > 0) {
+          addCarouselCardTutorials(data);
+        }
+      });
+    });
+  }
+  
+  listPopularTutorials();
+
+
+  function addCarouselCardLatestVideos(data) {
+    const carouselInner = $("#carouselExampleControlsLatest .carousel-inner");
+
+    carouselInner.empty();
+
+    data.forEach((video, index) => {
+        const activeClass = index === 0 ? 'active' : '';
+
+        const carouselItem = $(`
+            <div class="carousel-item ${activeClass}">
+                <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img src="${video.thumbnail_url}" class="card-img-top" alt="Video thumbnail">
+                        <div class="card-img-overlay text-center">
+                            <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold">${video.title}</h5>
+                            <p class="card-text text-muted">${video.sub_title}</p>
+                            <div class="creator d-flex align-items-center">
+                                <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                                <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                            </div>
+                            <div class="info pt-3 d-flex justify-content-between">
+                                <div class="rating">
+                                    ${'<img src="images/star_on.png" alt="star on" width="15px">'.repeat(video.star)}
+                                    ${'<img src="images/star_off.png" alt="star off" width="15px">'.repeat(5 - video.star)}
+                                </div>
+                                <span class="main-color">${video.duration}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+
+        carouselInner.append(carouselItem);
+    });
+}
+
+function listLatestVideos() {
+    const url = "https://smileschool-api.hbtn.info/latest-videos";
+
+    $(document).ready(function () {
+        $.ajax({
+            url: url,
+            method: "GET",
+            dataType: "json",
+            crossDomain: true,
+            beforeSend: function () {
+                $(".loader").show();  // Afficher le loader avant de commencer la requête
+            },
+            success: function () {
+                $(".loader").hide();  // Cacher le loader lorsque les données sont reçues
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error("Error fetching data:", textStatus, errorThrown);
+                $(".loader").hide();  // Cacher le loader même en cas d'erreur
+            }
+        }).done(function (data) {
+            if (Array.isArray(data) && data.length > 0) {
+                addCarouselCardLatestVideos(data);
+            }
+        });
+    });
+}
+
+listLatestVideos();
+
